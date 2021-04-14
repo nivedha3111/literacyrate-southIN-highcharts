9d0e6b8b8125fb644762ac2683a1ae5f7e89a49f
nivedha3111 <nivedhakesav@gmail.com>
2021-04-14 16:21:41 +0530

    last

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def read_json():
         #'local_data': data,
         #'series_names': 'series names',
        # 'title': 'netflix comedy series and their IMDb rating ',
-       'subtitle': 'Source: https://www.netflix.com/in/browse/genre/10375',
+       'subtitle': 'Source: https://www.epw.in/journal/2019/48/special-articles/literacy-achievement-india.html',
         #'rate_data': rate_list
     }
 
